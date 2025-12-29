ff8fb7def961a0d9cab2c5e2d3d9b367b010a8a9
Daniel Booth <user@danielbooth.cloud>
2025-12-29 12:00:44 +0000

    style: zelllij theme

--- dot_config/nvim/lua/custom/plugins/autopairs.lua
+++ dot_config/nvim/lua/custom/plugins/autopairs.lua
@@ -1,12 +1,20 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {
-    check_ts = true, -- Enable treesitter integration
-    ts_config = {
-      lua = { 'string' }, -- Don't add pairs in lua string treesitter nodes
-      javascript = { 'template_string' },
-    },
-    disable_filetype = { 'TelescopePrompt', 'vim' },
-  },
+  config = function()
+    local npairs = require('nvim-autopairs')
+    npairs.setup({
+      check_ts = true, -- Enable treesitter integration
+      ts_config = {
+        lua = { 'string' }, -- Don't add pairs in lua string treesitter nodes
+        javascript = { 'template_string' },
+      },
+      disable_filetype = { 'TelescopePrompt', 'vim' },
+    })
+
+    -- Remove quote autopairs
+    local Rule = require('nvim-autopairs.rule')
+    npairs.remove_rule("'")
+    npairs.remove_rule('"')
+  end,
 }
