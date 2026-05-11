09272a5f4451ba7c6300868582c81155f80bd2ab
Daniel Booth <user@danielbooth.cloud>
2026-05-11 16:53:42 +0100

    feat: rename to eternal off and on and correct ded preview

--- dot_config/fish/functions/ded.fish
+++ dot_config/fish/functions/ded.fish
@@ -1,6 +1,6 @@
 function ded
   set -l selected (chezmoi managed -i files | fzf \
-    --preview='bat --color=always --theme base16 "$HOME/{}"')
+    --preview="bat --color=always --theme base16 $HOME/{}")
   or return
 
   chezmoi edit "$HOME/$selected"; and chezmoi apply "$HOME/$selected" -v --no-pager
