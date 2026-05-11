9159c1b4426792dbf78f1abd3364f27e3246239b
Daniel Booth <user@danielbooth.cloud>
2026-05-11 13:01:23 +0100

    feat: update packages

--- dot_config/fish/conf.d/zaliases.fish
+++ dot_config/fish/conf.d/zaliases.fish
@@ -1,16 +1,11 @@
 status is-interactive; or exit 0
 
 alias istio="istioctl"
-alias mk="minikube"
-alias dc="docker-compose"
-alias tf="terraform"
 alias vi="nvim"
 alias pm="pnpm"
 alias px="pnpx"
 alias tpe="telepresence"
 alias docker="podman"
-alias gci="cz"
-alias tg="terragrunt"
 alias dotfiles="chezmoi"
 alias ll="eza --color=always --long --git --icons=always --no-user"
 alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
@@ -23,3 +18,6 @@ alias assume="source (brew --prefix)/bin/assume.fish"
 
 abbr -e gup
 abbr -a kb "kubectl"
+abbr -a tf "terraform"
+abbr -a mk "minikube"
+abbr -a tg "terragrunt"
